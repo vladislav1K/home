2055d4fb368c8c6205720254acaec9a9fbbd82b8
Vladislav Kroshkin <Vladislav_Kroshkin@epam.com>
2016-05-10 18:15:33 +0300

    all

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    var button = document.querySelector('.menu__references--ab');
+    var tegs = document.querySelector('.tegs__tablet');
+    
+    button.onclick = function dropButton() {
+       tegs.style.marginTop = '4em';
+        
+        
+    }
+}
+
+
+    
